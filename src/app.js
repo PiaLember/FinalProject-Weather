6b58196bc8d1d8c6f7f082e26c45d315ec521846
PiaLember <pia.lember@gmail.com>
2020-12-01 11:38:42 -0600

    added weather display in js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,22 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temp");
+  let cityElement = document.querySelector("#search-outcome");
+  let descriptionElement = document.querySelector("#description");
+  let realFeelElement = document.querySelector("#real-feel");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = `${Math.round(response.data.main.temp)}°`;
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  realFeelElement.innerHTML = `Real feel ${Math.round(
+    response.data.main.feels_like
+  )}°`;
+  humidityElement.innerHTML = `Humidity: ${response.data.main.humidity} %`;
+  windElement.innerHTML = `Wind: ${Math.round(response.data.wind.speed)} m/s`;
+}
+
 let apiKey = "ac0f4954f2276c6ad1120e7edce5fa23";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Lewisville&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
